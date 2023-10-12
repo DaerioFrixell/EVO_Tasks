6f81cd39a992b6cfdd682bd0edd948cbb2e068d1
Denis Gorbunov <anywaymaybe1@gmail.com>
2023-10-12 16:47:41 +0400

    create TS files, upload validateFormDate

--- index.ts
+++ index.ts
@@ -21,21 +21,20 @@ input.addEventListener("input", function (e) {
 	const regexp = /([0-3] | 0[1-9])/
 	let { value } = e.target
 	// const a = regexp.test(value)
-
 	if ((value[0] === "0" && value[1] === "0") || (value[0] === "3" && Number(value[1]) > 1)) {
-		input.value = value[0] + "1" + value.slice(2)
-	}
-	if (value[3] === "0" && value[4] === "0") input.value = value.slice(0, 4) + "1" + value.slice(5)
-	if (value[3] === "1" && Number(value[4]) > 2) input.value = value.slice(0, 4) + "2" + value.slice(5)
+		input.value = value[0] + "1"
+	} 
+	if (value[3] + value[4] === "00")  input.value = value.slice(0, 3) + "01"
+	if (Number(value[3] + value[4]) > 12) input.value = value.slice(0, 3) + "12"
 	if (value[6] === "0") input.value = value.slice(0, 6) + "1"
 	if (value[6] === "1" && Number(value[7]) < 9) input.value = value.slice(0, 7) + "9"
-
-	if (value[6] === "1" && value[7] === "9" && Number(value[8]) < 6) input.value = value.slice(0, 8) + "6"
+  if (value[6] + value[7] === "19" && Number(value[8]) < 6) input.value = value.slice(0, 8) + "6"
 
 	/**
 	 * если больше 29 числа и год какой-то там
 	 */
 	const visYear = Number(value[6] + value[7] + value[8] + value[9])
+	
 	if (
 		((value[0] === "2" && value[1] === "9") || (value[0] === "3" && Number(value[1]) < 2)) &&
 		value[3] === "0" &&
@@ -59,12 +58,7 @@ input.addEventListener("input", function (e) {
  * form.date = input.value
  * date = string in class method
  */
-if ((date[0] === "0" && date[1] === "0") || (date[0] === "3" && Number(date[1]) > 1)) {
-	this.formatDate = date[0] + "1" + date.slice(2)
-}
 
-if (date[3] + date[4] === "00") this.formatDate = date.slice(0, 3) + "01" + date.slice(5)
-if (Number(date[3] + date[4]) > 12) this.formatDate = date.slice(0, 3) + "12" + date.slice(5)
 
 if (date[6] === "0") this.formatDate = date.slice(0, 6) + "1"
 if (date[6] === "1" && Number(date[7]) < 9) this.formatDate = date.slice(0, 7) + "9"
