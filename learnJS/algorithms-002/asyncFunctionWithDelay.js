ff05825df36da9e5da6e6f4f1715ad46bb30b1c8
denis <anywaymaybe1@gmail.com>
2024-09-13 05:53:46 +0400

    added the first algorithmic task

---
+++ learnJS/algorithms-002/asyncFunctionWithDelay.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const firstFn = () => console.log("first");
+const secondFn = () => console.log("second");
+const thirdFn = () => console.log("third");
+
+const functionsArray = [firstFn, secondFn, thirdFn];
+const delays = [2000, 1000, 3000];
+
+const runInOrder = function (functionsArray, delays) {
+  (function loopIt(i) {
+    setTimeout(function () {
+      functionsArray[i]();
+
+      if (i < functionsArray.length - 1) loopIt(i + 1);
+    }, delays[i]);
+  })(0);
+};
+
+runInOrder(functionsArray, delays);
